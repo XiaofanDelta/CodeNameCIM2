f1b67ee5e4347e5ccfc662480d3783e67a9c1806
Qi-Month <dy757410@163.com>
2026-01-10 15:37:45 +0800

    Modifier.js

--- kubejs/server_scripts/recipes/tconstruct/Modifier.js
+++ kubejs/server_scripts/recipes/tconstruct/Modifier.js
@@ -1,58 +1,60 @@
 ServerEvents.recipes((event) => {
+	/** 
+		* 
+		* @param {String} modifier
+		* @param {Set} inputs
+		* @param {String} targetTag 
+		* @returns
+	   */
+	function addModifierRecipe(modifier, inputs, targetTag) {
+		let recipe = {
+			"type": "tconstruct:modifier",
+			"allow_crystal": true,
+			"check_trait_level": true,
+			"inputs": inputs,
+			"level": 1,
+			"result": modifier,
+			"slots": {
+				"abilities": 1
+			},
+			"tools": Ingredient.of(targetTag).toJson()
+		}
+		return event.custom(recipe)
+	}
 
-    /** 
-        * 
-        * @param {String} modifier
-        * @param {Set} inputs
-        * @param {String} targetTag 
-        * @returns
-       */
+	/**
+	 * 
+	 * @param {Internal.Ingredient_} input 
+	 * @returns 
+	 */
+	function setInput(input) {
+		return Ingredient.of(input).toJson()
+	}
 
-    function addCustomModifierRecipe(modifier, inputs, targetTag) {
-        let recipe = {
-            "type": "tconstruct:modifier",
-            "allow_crystal": true,
-            "check_trait_level": true,
-            "inputs": inputs,
-            "level": 1,
-            "result": modifier,
-            "slots": {
-                "abilities": 1
-            },
-            "tools": {
-                "tag": targetTag
-            }
-        }
-        return event.custom(recipe)
-    }
+	addModifierRecipe("nebula_tinker:acupoint", [
+		setInput("cmi:blackstone_source_alpha"),
+		setInput("cmi:blackstone_source_beta"),
+		setInput("cmi:blackstone_source_gamma"),
+		setInput("#forge:gems/charged_amethyst"),
+		setInput("#forge:gems/charged_amethyst")
+	], "#tconstruct:modifiable/held")
+		.id("nebula_tinker:tconstruct/modifier/ability/acupoint")
 
-    addCustomModifierRecipe("nebula_tinker:acupoint", [
-        { "item": "cmi:blackstone_source_alpha" },
-        { "item": "cmi:blackstone_source_beta" },
-        { "item": "cmi:blackstone_source_gamma" },
-        { "tag": "forge:gems/charged_amethyst" },
-        { "tag": "forge:gems/charged_amethyst" }
-    ],
-        "tconstruct:modifiable/held"
-    ).id("nebula_tinker:tconstruct/modifier/ability/acupoint")
+	addModifierRecipe("nebula_tinker:frenzy", [
+		setInput("#forge:dusts/quartz"),
+		setInput("#create:mechanisms/cobalt"),
+		setInput("#forge:dusts/quartz"),
+		setInput("#forge:gems/charged_amethyst"),
+		setInput("#forge:gems/charged_amethyst")
+	], "#tconstruct:modifiable/held")
+		.id("nebula_tinker:tconstruct/modifier/ability/frenzy")
 
-    addCustomModifierRecipe("nebula_tinker:frenzy", [
-        { "tag": "forge:dusts/quartz" },
-        { "tag": "create:mechanisms/cobalt" },
-        { "tag": "forge:dusts/quartz" },
-        { "tag": "forge:gems/charged_amethyst" },
-        { "tag": "forge:gems/charged_amethyst" }
-    ],
-        "tconstruct:modifiable/held"
-    ).id("nebula_tinker:tconstruct/modifier/ability/frenzy")
-
-    addCustomModifierRecipe("nebula_tinker:causal_truncation", [
-        { "tag": "forge:plates/uranium" },
-        { "tag": "create:mechanisms/nether" },
-        { "tag": "forge:plates/uranium" },
-        { "tag": "forge:slimeball/blood" },
-        { "tag": "forge:slimeball/blood" }
-    ],
-        "tconstruct:modifiable/melee/primary"
-    ).id("nebula_tinker:tconstruct/modifier/ability/causal_truncation")
+	addModifierRecipe("nebula_tinker:causal_truncation", [
+		setInput("#forge:plates/uranium"),
+		setInput("#create:mechanisms/nether"),
+		setInput("#forge:plates/uranium"),
+		setInput("#forge:slimeball/blood"),
+		setInput("#forge:slimeball/blood")
+	], "#tconstruct:modifiable/melee/primary")
+		.id("nebula_tinker:tconstruct/modifier/ability/causal_truncation")
 })
