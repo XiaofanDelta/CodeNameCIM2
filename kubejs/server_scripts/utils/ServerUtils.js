6ec27535208a38c985cef9c220dcc1936d001706
Eternal-Snowstorm <3512546747@qq.com>
2026-02-05 21:03:51 +0100

    转移

--- kubejs/server_scripts/utils/ServerUtils.js
+++ kubejs/server_scripts/utils/ServerUtils.js
@@ -1,164 +1,187 @@
-const MECHANISM = {
+let Mechanism = Object.freeze({
     // C0
-    WOODEN: {
-        COM: "cmi:wooden_mechanism",
-        INC: "cmi:incomplete_wooden_mechanism"
-    },
-    STONE: {
-        COM: "cmi:stone_mechanism",
-        INC: "cmi:incomplete_stone_mechanism"
-    },
-    REDSTONE: {
-        COM: "vintageimprovements:redstone_module",
-        INC: "vintageimprovements:incomplete_redstone_module"
-    },
-    NATURE: {
-        COM: "cmi:nature_mechanism",
-        INC: "cmi:incomplete_nature_mechanism"
-    },
+    WOODEN: Object.freeze({
+        COM: Item.of("cmi:wooden_mechanism"),
+        INC: Item.of("cmi:incomplete_wooden_mechanism")
+    }),
+    STONE: Object.freeze({
+        COM: Item.of("cmi:stone_mechanism"),
+        INC: Item.of("cmi:incomplete_stone_mechanism")
+    }),
+    REDSTONE: Object.freeze({
+        COM: Item.of("vintageimprovements:redstone_module"),
+        INC: Item.of("vintageimprovements:incomplete_redstone_module")
+    }),
+    NATURE: Object.freeze({
+        COM: Item.of("cmi:nature_mechanism"),
+        INC: Item.of("cmi:incomplete_nature_mechanism")
+    }),
     // C1
-    ANDESITE: {
-        COM: "cmi:andesite_mechanism",
-        INC: "cmi:incomplete_andesite_mechanism"
-    },
-    COPPER: {
-        COM: "cmi:copper_mechanism",
-        INC: "cmi:incomplete_copper_mechanism"
-    },
-    IRON: {
-        COM: "cmi:iron_mechanism",
-        INC: "cmi:incomplete_iron_mechanism"
-    },
-    PIG_IRON: {
-        COM: "cmi:pig_iron_mechanism",
-        INC: "cmi:incomplete_pig_iron_mechanism"
-    },
-    POTION: {
-        COM: "cmi:potion_mechanism",
-        INC: "cmi:incomplete_potion_mechanism"
-    },
+    ANDESITE: Object.freeze({
+        COM: Item.of("cmi:andesite_mechanism"),
+        INC: Item.of("cmi:incomplete_andesite_mechanism"),
+        BAS: Item.of("cmi:andesite_mechanism_basement")
+
+    }),
+    COPPER: Object.freeze({
+        COM: Item.of("cmi:copper_mechanism"),
+        INC: Item.of("cmi:incomplete_copper_mechanism"),
+        BAS: Item.of("cmi:copper_mechanism_basement")
+    }),
+    IRON: Object.freeze({
+        COM: Item.of("cmi:iron_mechanism"),
+        INC: Item.of("cmi:incomplete_iron_mechanism"),
+        BAS: Item.of("cmi:iron_mechanism_basement")
+    }),
+    PIG_IRON: Object.freeze({
+        COM: Item.of("cmi:pig_iron_mechanism"),
+        INC: Item.of("cmi:incomplete_pig_iron_mechanism")
+    }),
+    POTION: Object.freeze({
+        COM: Item.of("cmi:potion_mechanism"),
+        INC: Item.of("cmi:incomplete_potion_mechanism")
+    }),
     // C2
-    STEAM: {
-        COM: "cmi:bronze_mechanism",
-        INC: "cmi:incomplete_bronze_mechanism"
-    },
-    RAILWAY: {
-        COM: "cmi:railway_mechanism",
-        INC: "cmi:incomplete_railway_mechanism"
-    },
-    NETHER: {
-        COM: "cmi:nether_mechanism",
-        INC: "cmi:incomplete_nether_mechanism"
-    },
-    ENCHANTED: {
-        COM: "cmi:enchanted_mechanism",
-        INC: "cmi:incomplete_enchanted_mechanism"
-    },
-    GOLD: {
-        COM: "cmi:gold_mechanism",
-        INC: "cmi:incomplete_gold_mechanism"
-    },
+    STEAM: Object.freeze({
+        COM: Item.of("cmi:bronze_mechanism"),
+        INC: Item.of("cmi:incomplete_bronze_mechanism")
+    }),
+    RAILWAY: Object.freeze({
+        COM: Item.of("cmi:railway_mechanism"),
+        INC: Item.of("cmi:incomplete_railway_mechanism")
+    }),
+    NETHER: Object.freeze({
+        COM: Item.of("cmi:nether_mechanism"),
+        INC: Item.of("cmi:incomplete_nether_mechanism")
+    }),
+    ENCHANTED: Object.freeze({
+        COM: Item.of("cmi:enchanted_mechanism"),
+        INC: Item.of("cmi:incomplete_enchanted_mechanism")
+    }),
+    GOLD: Object.freeze({
+        COM: Item.of("cmi:gold_mechanism"),
+        INC: Item.of("cmi:incomplete_gold_mechanism")
+    }),
     //C3
-    COIL: {
-        COM: "cmi:coil_mechanism",
-        INC: "cmi:incomplete_coil_mechanism"
-    },
-    PHOTO: {
-        COM: "cmi:photosensitive_mechanism",
-        INC: "cmi:incomplete_photosensitive_mechanism"
-    },
-    PRECISION: {
-        COM: "create:precision_mechanism",
-        INC: "create:incomplete_precision_mechanism"
-    },
-    COBALT: {
-        COM: "cmi:cobalt_mechanism",
-        INC: "cmi:incomplete_cobalt_mechanism"
-    },
-    COLOR: {
-        COM: "cmi:colorful_mechanism",
-        INC: "cmi:incomplete_colorful_mechanism"
-    },
+    COIL: Object.freeze({
+        COM: Item.of("cmi:coil_mechanism"),
+        INC: Item.of("cmi:incomplete_coil_mechanism"),
+        AUG: Item.of("cmi:coil_mechanism_augment")
+    }),
+    PHOTO: Object.freeze({
+        COM: Item.of("cmi:photosensitive_mechanism"),
+        INC: Item.of("cmi:incomplete_photosensitive_mechanism")
+    }),
+    PRECISION: Object.freeze({
+        COM: Item.of("create:precision_mechanism"),
+        INC: Item.of("create:incomplete_precision_mechanism")
+    }),
+    COBALT: Object.freeze({
+        COM: Item.of("cmi:cobalt_mechanism"),
+        INC: Item.of("cmi:incomplete_cobalt_mechanism")
+    }),
+    COLOR: Object.freeze({
+        COM: Item.of("cmi:colorful_mechanism"),
+        INC: Item.of("cmi:incomplete_colorful_mechanism")
+    }),
     // C4
-    THERMAL: {
-        COM: "cmi:thermal_mechanism",
-        INC: "cmi:incomplete_thermal_mechanism"
-    },
-    LIGHT: {
-        COM: "cmi:light_engineering_mechanism",
-        INC: "cmi:incomplete_light_engineering_mechanism"
-    },
-    SMART: {
-        COM: "cmi:smart_mechanism",
-        INC: "cmi:incomplete_smart_mechanism"
-    },
-    SCULK: {
-        COM: "cmi:sculk_mechanism",
-        INC: "cmi:incomplete_sculk_mechanism"
-    },
+    THERMAL: Object.freeze({
+        COM: Item.of("cmi:thermal_mechanism"),
+        INC: Item.of("cmi:incomplete_thermal_mechanism"),
+        AUG: Item.of("cmi:thermal_mechanism_augment")
+    }),
+    LIGHT: Object.freeze({
+        COM: Item.of("cmi:light_engineering_mechanism"),
+        INC: Item.of("cmi:incomplete_light_engineering_mechanism"),
+        AUG: Item.of("cmi:light_engineering_mechanism_augment")
+    }),
+    SMART: Object.freeze({
+        COM: Item.of("cmi:smart_mechanism"),
+        INC: Item.of("cmi:incomplete_smart_mechanism"),
+        AUG: Item.of("cmi:smart_mechanism_augment")
+    }),
+    SCULK: Object.freeze({
+        COM: Item.of("cmi:sculk_mechanism"),
+        INC: Item.of("cmi:incomplete_sculk_mechanism")
+    }),
     // C5
-    REINFORCED: {
-        COM: "cmi:reinforced_mechanism",
-        INC: "cmi:incomplete_reinforced_mechanism"
-    },
-    HEAVY: {
-        COM: "cmi:heavy_engineering_mechanism",
-        INC: "cmi:incomplete_heavy_engineering_mechanism"
-    },
-    ENDER: {
-        COM: "cmi:ender_mechanism",
-        INC: "cmi:incomplete_ender_mechanism"
-    },
+    REINFORCED: Object.freeze({
+        COM: Item.of("cmi:reinforced_mechanism"),
+        INC: Item.of("cmi:incomplete_reinforced_mechanism"),
+        AUG: Item.of("cmi:reinforced_mechanism_augment")
+    }),
+    HEAVY: Object.freeze({
+        COM: Item.of("cmi:heavy_engineering_mechanism"),
+        INC: Item.of("cmi:incomplete_heavy_engineering_mechanism"),
+        AUG: Item.of("cmi:heavy_engineering_mechanism_augment")
+    }),
+    ENDER: Object.freeze({
+        COM: Item.of("cmi:ender_mechanism"),
+        INC: Item.of("cmi:incomplete_ender_mechanism"),
+        BAS: Item.of("cmi:ender_mechanism_basement")
+    }),
     // C6
-    BASIC: {
-        COM: "cmi:basic_mekanism_mechanism",
-        INC: "cmi:incomplete_basic_mekanism_mechanism"
-    },
-    AVI1: {
-        COM: "cmi:tier_1_aviation_mechanism",
-        INC: "cmi:incomplete_tier_1_aviation_mechanism"
-    },
-    COMPUTE: {
-        COM: "cmi:computing_mechanism",
-        INC: "cmi:incomplete_computing_mechanism"
-    },
+    BASIC: Object.freeze({
+        COM: Item.of("cmi:basic_mekanism_mechanism"),
+        INC: Item.of("cmi:incomplete_basic_mekanism_mechanism"),
+        BAS: Item.of("cmi:basic_mekanism_mechanism_basement")
+    }),
+    AVI1: Object.freeze({
+        COM: Item.of("cmi:tier_1_aviation_mechanism"),
+        INC: Item.of("cmi:incomplete_tier_1_aviation_mechanism"),
+        BAS: Item.of("cmi:tier_1_aviation_mechanism_basement")
+    }),
+    COMPUTE: Object.freeze({
+        COM: Item.of("cmi:computing_mechanism"),
+        INC: Item.of("cmi:incomplete_computing_mechanism"),
+        BAS: Item.of("cmi:computing_mechanism_basement")
+    }),
     // C7
-    ADVANCED: {
-        COM: "cmi:advanced_mekanism_mechanism",
-        INC: "cmi:incomplete_advanced_mekanism_mechanism"
-    },
-    AVI2: {
-        COM: "cmi:tier_2_aviation_mechanism",
-        INC: "cmi:incomplete_tier_2_aviation_mechanism"
-    },
-    AIR: {
-        COM: "cmi:air_tight_mechanism",
-        INC: "cmi:incomplete_air_tight_mechanism"
-    },
+    ADVANCED: Object.freeze({
+        COM: Item.of("cmi:advanced_mekanism_mechanism"),
+        INC: Item.of("cmi:incomplete_advanced_mekanism_mechanism"),
+        BAS: Item.of("cmi:advanced_mekanism_mechanism_basement")
+    }),
+    AVI2: Object.freeze({
+        COM: Item.of("cmi:tier_2_aviation_mechanism"),
+        INC: Item.of("cmi:incomplete_tier_2_aviation_mechanism"),
+        BAS: Item.of("cmi:tier_2_aviation_mechanism_basement")
+    }),
+    AIR: Object.freeze({
+        COM: Item.of("cmi:air_tight_mechanism"),
+        INC: Item.of("cmi:incomplete_air_tight_mechanism"),
+        BAS: Item.of("cmi:air_tight_mechanism_basement")
+    }),
     // C8
-    ELITE: {
-        COM: "cmi:elite_mekanism_mechanism",
-        INC: "cmi:incomplete_elite_mekanism_mechanism"
-    },
-    NUKE: {
-        COM: "cmi:nuclear_mechanism",
-        INC: "cmi:incomplete_nuclear_mechanism"
-    },
-    AVI3: {
-        COM: "cmi:tier_3_aviation_mechanism",
-        INC: "cmi:incomplete_tier_3_aviation_mechanism"
-    },
+    ELITE: Object.freeze({
+        COM: Item.of("cmi:elite_mekanism_mechanism"),
+        INC: Item.of("cmi:incomplete_elite_mekanism_mechanism"),
+        BAS: Item.of("cmi:elite_mekanism_mechanism_basement")
+    }),
+    NUKE: Object.freeze({
+        COM: Item.of("cmi:nuclear_mechanism"),
+        INC: Item.of("cmi:incomplete_nuclear_mechanism"),
+        BAS: Item.of("cmi:nuclear_mechanism_basement")
+    }),
+    AVI3: Object.freeze({
+        COM: Item.of("cmi:tier_3_aviation_mechanism"),
+        INC: Item.of("cmi:incomplete_tier_3_aviation_mechanism"),
+        BAS: Item.of("cmi:tier_3_aviation_mechanism_basement")
+    }),
     // C9
-    ULTIMATE: {
-        COM: "cmi:ultimate_mekanism_mechanism",
-        INC: "cmi:incomplete_ultimate_mekanism_mechanism"
-    },
-    AVI4: {
-        COM: "cmi:tier_4_aviation_mechanism",
-        INC: "cmi:incomplete_tier_4_aviation_mechanism"
-    },
-    ANTI: {
-        COM: "cmi:antimatter_mechanism",
-        INC: "cmi:incomplete_antimatter_mechanism"
-    }
-}
+    ULTIMATE: Object.freeze({
+        COM: Item.of("cmi:ultimate_mekanism_mechanism"),
+        INC: Item.of("cmi:incomplete_ultimate_mekanism_mechanism"),
+        BAS: Item.of("cmi:ultimate_mekanism_mechanism_basement")
+    }),
+    AVI4: Object.freeze({
+        COM: Item.of("cmi:tier_4_aviation_mechanism"),
+        INC: Item.of("cmi:incomplete_tier_4_aviation_mechanism"),
+        BAS: Item.of("cmi:tier_4_aviation_mechanism_basement")
+    }),
+    ANTI: Object.freeze({
+        COM: Item.of("cmi:antimatter_mechanism"),
+        INC: Item.of("cmi:incomplete_antimatter_mechanism"),
+        BAS: Item.of("cmi:antimatter_mechanism_basement")
+    })
+})
