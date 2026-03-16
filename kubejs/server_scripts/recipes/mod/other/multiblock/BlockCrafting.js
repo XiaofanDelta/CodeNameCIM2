6a21bd9ae28d512d9325662b8dcec0e5430a1a8b
Qi-Month <dy757410@163.com>
2026-03-16 17:11:41 +0800

    嗯, 应该可以了

--- kubejs/server_scripts/recipes/mod/other/multiblock/BlockCrafting.js
+++ kubejs/server_scripts/recipes/mod/other/multiblock/BlockCrafting.js
@@ -1,4 +1,4 @@
-let structure = new MultiblockStructureBuilder([
+let stru1 = new MultiblockStructureBuilder([
 	[
 		"   ",
 		"   ",
@@ -22,6 +22,17 @@ let structure = new MultiblockStructureBuilder([
 	.build()
 
 BlockEvents.rightClicked((event) => {
+	spawnEntityByMultiblock(event, stru1, "alexscaves:submarine")
+})
+
+/**
+ * @param {Internal.BlockRightClickedEventJS} event 
+ * @param {vazkii.patchouli.api.IMultiblock} structure 
+ * @param {Internal.EntityType_<any>} entityId 
+ * @returns 
+ */
+
+function spawnEntityByMultiblock(event, structure, entityId) {
 	let { player, block, level } = event
 
 	if (event.hand !== InteractionHand.MAIN_HAND) {
@@ -38,13 +49,40 @@ BlockEvents.rightClicked((event) => {
 		.cacheTicks(20)
 		.build()
 
-	console.log(multiblock.getDirection())
+	let direction = multiblock.getDirection()
+
 	player.swing()
 
 	if (!player.isCreative()) {
 		multiblock.destroyAll(false)
 	}
-	let entity = level.createEntity("immersive_machinery:copperfin")
+
+	let entity = level.createEntity(entityId)
+
 	entity.setPos(block.x + 0.5, block.y, block.z + 0.5)
+
 	entity.spawn()
-})
+
+	if (direction) {
+		let rot = directionToYRot(direction)
+
+		entity.setYRot(rot)
+		entity.setYHeadRot(rot)
+		entity.setYBodyRot(rot)
+	}
+}
+
+/**
+ * 
+ * @param {Internal.Direction} direction 
+ * @returns 
+ */
+function directionToYRot(direction) {
+	switch (direction.name()) {
+		case "NORTH": return 180
+		case "SOUTH": return 0
+		case "WEST": return 90
+		case "EAST": return -90
+		default: return 0
+	}
+}
