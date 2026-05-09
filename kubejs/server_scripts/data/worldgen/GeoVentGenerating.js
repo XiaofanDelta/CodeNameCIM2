3f9f64217be2a559a945d30a51610de08e84abc3
Qi-Month <dy757410@163.com>
2026-05-09 21:32:53 +0800

    格式化

--- kubejs/server_scripts/data/worldgen/GeoVentGenerating.js
+++ kubejs/server_scripts/data/worldgen/GeoVentGenerating.js
@@ -1,88 +1,88 @@
 ServerEvents.highPriorityData((event) => {
-    /** 
-     * 
-     * @param {String} name 
-     * @param {String} bio 
-     * @returns
-    */
+	/** 
+	 * 
+	 * @param {String} name 
+	 * @param {String} bio 
+	 * @returns
+	*/
 
-    function createVentGenerate(name, salt, bio) {
-        let structure = {
-            type: "minecraft:jigsaw",
-            biomes: bio,
-            size: 1,
-            start_pool: `${Cmi.MODID}:geo_vent/${name}_geo_vent`,
-            step: "underground_structures",
-            start_height: {
-                absolute: 0
-            },
-            spawn_overrides: {},
-            max_distance_from_center: 64,
-            project_start_to_heightmap: "WORLD_SURFACE_WG",
-            terrain_adaptation: "bury",
-            use_expansion_hack: false
-        }
+	function createVentGenerate(name, salt, bio) {
+		let structure = {
+			type: "minecraft:jigsaw",
+			biomes: bio,
+			size: 1,
+			start_pool: `${Cmi.MODID}:geo_vent/${name}_geo_vent`,
+			step: "underground_structures",
+			start_height: {
+				absolute: 0
+			},
+			spawn_overrides: {},
+			max_distance_from_center: 64,
+			project_start_to_heightmap: "WORLD_SURFACE_WG",
+			terrain_adaptation: "bury",
+			use_expansion_hack: false
+		}
 
-        let structureSet = {
-            structures: [
-                {
-                    structure: `${Cmi.MODID}:geo_vent/${name}_geo_vent`,
-                    weight: 1
-                }
-            ],
-            placement: {
-                type: "minecraft:random_spread",
-                salt: salt,
-                spacing: 70,
-                separation: 50
-            }
-        }
+		let structureSet = {
+			structures: [
+				{
+					structure: `${Cmi.MODID}:geo_vent/${name}_geo_vent`,
+					weight: 1
+				}
+			],
+			placement: {
+				type: "minecraft:random_spread",
+				salt: salt,
+				spacing: 70,
+				separation: 50
+			}
+		}
 
-        let templatePool = {
-            name: `${Cmi.MODID}:${name}_geo_vent`,
-            fallback: "minecraft:empty",
-            elements: [
-                {
-                    weight: 1,
-                    element: {
-                        location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_0`,
-                        element_type: "minecraft:single_pool_element",
-                        processors: "minecraft:empty",
-                        projection: "terrain_matching"
-                    }
-                }, {
-                    weight: 1,
-                    element: {
-                        location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_1`,
-                        element_type: "minecraft:single_pool_element",
-                        processors: "minecraft:empty",
-                        projection: "terrain_matching"
-                    }
-                }, {
-                    weight: 1,
-                    element: {
-                        location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_2`,
-                        element_type: "minecraft:single_pool_element",
-                        processors: "minecraft:empty",
-                        projection: "terrain_matching"
-                    }
-                }, {
-                    weight: 1,
-                    element: {
-                        location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_3`,
-                        element_type: "minecraft:single_pool_element",
-                        processors: "minecraft:empty",
-                        projection: "terrain_matching"
-                    }
-                }
-            ]
-        }
+		let templatePool = {
+			name: `${Cmi.MODID}:${name}_geo_vent`,
+			fallback: "minecraft:empty",
+			elements: [
+				{
+					weight: 1,
+					element: {
+						location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_0`,
+						element_type: "minecraft:single_pool_element",
+						processors: "minecraft:empty",
+						projection: "terrain_matching"
+					}
+				}, {
+					weight: 1,
+					element: {
+						location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_1`,
+						element_type: "minecraft:single_pool_element",
+						processors: "minecraft:empty",
+						projection: "terrain_matching"
+					}
+				}, {
+					weight: 1,
+					element: {
+						location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_2`,
+						element_type: "minecraft:single_pool_element",
+						processors: "minecraft:empty",
+						projection: "terrain_matching"
+					}
+				}, {
+					weight: 1,
+					element: {
+						location: `${Cmi.MODID}:geo_vent/${name}_geo_vent_3`,
+						element_type: "minecraft:single_pool_element",
+						processors: "minecraft:empty",
+						projection: "terrain_matching"
+					}
+				}
+			]
+		}
 
-        event.addJson(`cmi:worldgen/structure/geo_vent/${name}_geo_vent`, structure)
-        event.addJson(`cmi:worldgen/structure_set/geo_vent/${name}_geo_vent`, structureSet)
-        event.addJson(`cmi:worldgen/template_pool/geo_vent/${name}_geo_vent`, templatePool)
-    }
+		event.addJson(`cmi:worldgen/structure/geo_vent/${name}_geo_vent`, structure)
+		event.addJson(`cmi:worldgen/structure_set/geo_vent/${name}_geo_vent`, structureSet)
+		event.addJson(`cmi:worldgen/template_pool/geo_vent/${name}_geo_vent`, templatePool)
+	}
 
-    createVentGenerate("mars", 1241241235, "ad_astra:martian_wastelands")
-    createVentGenerate("mercury", 78579128571, "ad_astra:mercury_deltas")
+	createVentGenerate("mars", 1241241235, "ad_astra:martian_wastelands")
+	createVentGenerate("mercury", 78579128571, "ad_astra:mercury_deltas")
 })
