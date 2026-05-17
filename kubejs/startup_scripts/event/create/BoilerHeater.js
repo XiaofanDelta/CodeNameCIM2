abfdcc9f3921524be8602cfd8e2851fb4597b5b3
Qi-Month <dy757410@163.com>
2026-05-17 21:16:25 +0800

    我懒得测试了

--- kubejs/startup_scripts/event/create/BoilerHeater.js
+++ kubejs/startup_scripts/event/create/BoilerHeater.js
@@ -5,7 +5,7 @@ CreateEvents.boilerHeatHandler((event) => {
 	event.add("cmi:bronze_fluid_burner", (block) => {
 		let state = block.getBlockState()
 
-		if (state.getValue($BlockStateProperties.LIT)) {
+		if (state.getValue($BlockStateProperties.LIT) === true) {
 			return 2
 		}
 		return 1
