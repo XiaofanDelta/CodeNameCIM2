2767ba0d60401fbd3402d55fcab88292aad35c61
Qi-Month <dy757410@163.com>
2026-07-14 16:39:55 +0800

    chemical_reactor

--- kubejs/server_scripts/recipes/mod/mbd2/proxy/ChemicalReactor.js
+++ kubejs/server_scripts/recipes/mod/mbd2/proxy/ChemicalReactor.js
@@ -5,3 +5,13 @@ MBDRecipeTypeEvents.onTransferProxyRecipe(($) => {
 	let event = $.getEvent()
 	let { proxyRecipe, proxyRecipeId, proxyTypeId, recipeType } = event
 })
+
+ServerEvents.recipes((event) => {
+	let { cmi } = event.getRecipes()
+
+	cmi.chemical_reactor()
+		// .inputFE(100)
+		.outputFluids(Fluid.of("minecraft:water", 100))
+		.inputFluids(Fluid.of("minecraft:lava", 100))
+		.inputItems("#minecraft:dirt")
+})
